80585c98248dc6a4cba4d46d0dafee3f268d2fa8
Adaxry <lyj14282008@gmail.com>
2019-06-06 12:02:00 +0800

    Update trim_glove.sh

--- trim_glove.sh
+++ trim_glove.sh
@@ -1,3 +1,7 @@
 glove_path=$1
+
+echo "Triming with the vocab of conll03"
 python ./thumt/utils/get_trimmed_glove.py $glove_path ./data/conll03/vocab.w ./data/conll03/eng.glove
+
+echo "Triming with the vocab of conll2000"
 python ./thumt/utils/get_trimmed_glove.py $glove_path ./data/conll2000/vocab.w ./data/conll2000/eng.glove
