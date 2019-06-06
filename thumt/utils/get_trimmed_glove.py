ebef16418e4066e6fa6ff7aa51c277893160e1b6
Adaxry <lyj14282008@gmail.com>
2019-06-06 12:02:57 +0800

    Update get_trimmed_glove.py

--- thumt/utils/get_trimmed_glove.py
+++ thumt/utils/get_trimmed_glove.py
@@ -1,12 +1,12 @@
-import sys
+import sys 
 import codecs
 
 with codecs.open(sys.argv[1], "r", encoding="utf-8") as emb_f, \
      codecs.open(sys.argv[2], "r", encoding="utf-8") as vocab_f, \
      codecs.open(sys.argv[3], "w", encoding="utf-8") as glove_f:
 
-    hit_num = 0
     vocab = set([word.strip() for word in vocab_f])
+    done_set = set()
     for line in emb_f:
         try:
             vector = line.strip().split()
@@ -16,11 +16,6 @@ with codecs.open(sys.argv[1], "r", encoding="utf-8") as emb_f, \
                 word = line.split()[0]
         except:
             continue
-        if word in vocab:
-            hit_num += 1
+        if word in vocab and not word in done_set:
+            done_set.add(word)
             glove_f.write(line)
-
-    print("Final hit num : ", hit_num)
-    print("Final hit rate : ", float(hit_num)/len(vocab))
-
-
